6251cf307474a51aa617ddcdae4cc448eaebf20c
Madhari Tarunsai <saitarunsai04062002@gmail.com>
2025-07-25 07:24:56 +0530

    updated

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,8 +1,9 @@
 const mongoose=require("mongoose")
+const uri="mongodb+srv://saitarunsai04062002:gfW6PmASedRpBQ3N@cluster0.annbira.mongodb.net/"
 // function creation
 async function conntect_dataBase(){
     try{
-        const mongodb=await mongoose.connect("mongodb://localhost:27017/",{dbName:"todo_application"})
+        const mongodb=await mongoose.connect(uri,{dbName:"todo_application"})
         console.log("database connected sucessfully")
 
     }catch(error){
